82c9f170c132e4b5fdb63ec874c78ac2e0a987fa
Shabas Rahman P <62629281+ShabasRahman7@users.noreply.github.com>
2025-09-09 10:50:17 +0530

    1019-SQUARES-OF-A-SORTED-ARRAY Stats: Time: 15 ms (23.55%), Space: 19.7 MB (22.94%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -1,13 +1,17 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        l,r = 0,len(nums)-1
-        result = []
-        while(l<=r):
-            left,right = abs(nums[l]),abs(nums[r])
-            if(left>right):
-                result.insert(0,left**2)
-                l+=1
+        length=len(nums)
+        left_pos=0
+        right_pos=length-1
+        insert_pos=-1
+        result = [0]*length
+        while(left_pos<=right_pos):
+            left_element,right_element =abs(nums[left_pos]),abs(nums[right_pos])
+            if(left_element>right_element):
+                result[insert_pos]=left_element**2
+                left_pos+=1
             else:
-                result.insert(0,right**2)
-                r-=1
+                result[insert_pos]=right_element**2
+                right_pos-=1
+            insert_pos-=1
         return result

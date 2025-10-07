c00c19e8d0220c051d313868ebac66ec81ae6f4f
Shabas Rahman P <62629281+ShabasRahman7@users.noreply.github.com>
2025-10-07 10:43:50 +0530

    3275-MINIMUM-NUMBER-OF-PUSHES-TO-TYPE-WORD-I Stats: Time: 0 ms (100%), Space: 17.9 MB (33.07%) - LeetHub

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        length=len(word)
+        i=1
+        count=0
+        while(length>0):
+            if(length-8>0):
+                count+=i*8
+                length-=8
+            else:
+                count+=i*length
+                length=0
+            i+=1
+        return count
+
+        

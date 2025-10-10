5f36af391f27d912ab7bf774414380b0df177ffd
Shabas Rahman P <62629281+ShabasRahman7@users.noreply.github.com>
2025-10-10 10:17:36 +0530

    0167-TWO-SUM-II-INPUT-ARRAY-IS-SORTED Stats: Time: 6 ms (28%), Space: 18.5 MB (59.45%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,7 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        hash={
-            numbers[0]:1
-        }
-        for i in range(1,len(numbers)):
-            print(hash)
+        hash={}
+        for i in range(0,len(numbers)):
             a=target-numbers[i]
             if a in hash.keys():
                 return [hash[a],i+1]

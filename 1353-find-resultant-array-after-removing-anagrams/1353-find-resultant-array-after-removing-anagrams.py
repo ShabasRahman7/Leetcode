f7c1ece3f27cc7630e71ab059d73665a8ae61c6b
Shabas Rahman P <62629281+ShabasRahman7@users.noreply.github.com>
2025-10-13 11:27:26 +0530

    1353-FIND-RESULTANT-ARRAY-AFTER-REMOVING-ANAGRAMS Stats: Time: 7 ms (50.59%), Space: 17.8 MB (76.67%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
@@ -2,10 +2,16 @@ class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
         length=len(words)-1
         i=1
+        def get_freq(word):
+            count=[0]*26
+            for c in word:
+                count[ord(c)-ord('a')]+=1
+            return tuple(count)
+            
         while(i<=length):
             if(len(words[i]) != len(words[i-1])):
                 i+=1
-            elif(Counter(words[i])==Counter(words[i-1])):
+            elif(get_freq(words[i]) == get_freq(words[i-1])):
                 words.pop(i)
                 length-=1
             else:

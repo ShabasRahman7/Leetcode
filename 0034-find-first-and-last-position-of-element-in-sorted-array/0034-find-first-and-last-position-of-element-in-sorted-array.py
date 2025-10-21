cccb0b61b7e0e85399a74e44ba3e40986778361e
Shabas Rahman P <62629281+ShabasRahman7@users.noreply.github.com>
2025-10-21 21:15:27 +0530

    0034-FIND-FIRST-AND-LAST-POSITION-OF-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 19 MB (75.15%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,25 +1,31 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        res =[-1]*2
-        left=0
-        right=len(nums)-1
-        while(left<=right):
-            mid=(left+right)//2
-            if(nums[mid]>target):
-                right=mid-1
-            elif(nums[mid]<target):
-                left=mid+1
-            elif(nums[mid]==target):
-                i,j=mid,mid
-                while(i>0 and nums[i-1]==nums[i]):
-                    print("i=",i)
-                    i-=1
+        def findFirst():
+            left,right =0,len(nums)-1
+            index=-1
+            while left<=right:
+                mid=(left+right)//2
+                if nums[mid]>=target:
+                    right = mid - 1
+                else:
+                    left=mid+1
+                if nums[mid]==target:
+                    index = mid
+            return index
 
-                while(j<len(nums)-1 and nums[j+1]==nums[j]):
-                    print("j=",j)
-                    j+=1
-                res[0],res[1] = i,j
-                break
-        return res
+        def findLast():
+            left,right=0,len(nums)-1
+            index=-1
+            while left<=right:
+                mid=(left+right)//2
+                if nums[mid]<=target:
+                    left=mid+1
+                else:
+                    right=mid-1
+                if nums[mid]==target:
+                    index=mid
+            return index
+
+        return [findFirst(),findLast()]
 
         

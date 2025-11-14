2c13c2e40f3c083bf5cc22643dc61dc204c102a3
Shabas Rahman P <62629281+ShabasRahman7@users.noreply.github.com>
2025-11-14 10:40:58 +0530

    1580-SHUFFLE-THE-ARRAY Stats: Time: 55 ms (75.34%), Space: 18 MB (41.51%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.py
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -2,12 +2,9 @@ class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         x=nums[:n]
         y=nums[n:]
-        l=len(x)*2
-        res=[0]*l
-        n=0
-        for i in range(l//2):
-            res[n]=x[i]
-            n+=1
-            res[n]=y[i]
-            n+=1
+        l=len(x)
+        res=[]
+        for i in range(l):
+            res.append(x[i])
+            res.append(y[i])
         return res
